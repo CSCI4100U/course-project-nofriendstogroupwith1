1b46f223259e72048a80b2ed89dc757b19364bfe
Alexander Naylor <alexandernaylor@outlook.com>
2022-12-04 16:41:42 -0500

    added post markers to map screen

--- group_project/lib/views/map_view.dart
+++ group_project/lib/views/map_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-
+import 'package:group_project/models/post.dart';
+import 'package:group_project/models/post_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:group_project/constants.dart';
 
 class MapView extends StatefulWidget {
@@ -11,30 +13,74 @@ class MapView extends StatefulWidget {
 }
 
 class _MapView extends State<MapView> {
+  late List<Post> posts;
+
+  void _showPost(DocumentReference reference) {
+    //TODO move to post_view
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(), //Note: The back button is automatically added by Navigator.push()
+      appBar:
+          AppBar(), //Note: The back button is automatically added by Navigator.push()
       body: Stack(
         children: [
-          FlutterMap(
-            options: MapOptions(
-              minZoom: 5,
-              maxZoom: 18,
-              zoom: 13,
-              center: AppConstants.defaultLocation,
-            ),
-            layers: [
-              TileLayerOptions(
-                urlTemplate:
-                    "https://api.mapbox.com/styles/v1/alexnayl/{mapStyleId}/tiles/256/{z}/{x}/{y}@2x?access_token={accessToken}",
-                additionalOptions: {
-                  'mapStyleId': AppConstants.mapBoxStyleId,
-                  'accessToken': AppConstants.mapBoxAccessToken,
-                },
-              ),
-            ],
-          ),
+          FutureBuilder(
+              future: PostModel().getAllPostsList(),
+              builder: ((context, snapshot) {
+                if (!snapshot.hasData) {
+                  //Loading screen
+                  return Container(
+                      alignment: Alignment.center,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: const [
+                          CircularProgressIndicator(),
+                          Text("Loading Posts")
+                        ],
+                      ));
+                }
+
+                //Main screeen
+                return FlutterMap(
+                  options: MapOptions(
+                    minZoom: 5,
+                    maxZoom: 18,
+                    zoom: 13,
+                    center: AppConstants.defaultLocation,
+                  ),
+                  layers: [
+                    TileLayerOptions(
+                      urlTemplate:
+                          "https://api.mapbox.com/styles/v1/alexnayl/{mapStyleId}/tiles/256/{z}/{x}/{y}@2x?access_token={accessToken}",
+                      additionalOptions: {
+                        'mapStyleId': AppConstants.mapBoxStyleId,
+                        'accessToken': AppConstants.mapBoxAccessToken,
+                      },
+                    ),
+                    MarkerLayerOptions(rotate: true, markers: [
+                      //post icons
+                      for (int i = 0; i < snapshot.data!.length; i++)
+                        Marker(
+                          height: 80,
+                          width: 80,
+                          point: snapshot.data![i].location ??
+                              AppConstants.defaultLocation,
+                          builder: (context) => IconButton(
+                              onPressed: (() =>
+                                  _showPost(snapshot.data![i].reference!)),
+                              icon: Icon(
+                                Icons.location_pin,
+                                color: Colors.blue,
+                                size: 45,
+                              )),
+                        )
+                    ])
+                  ],
+                );
+              }))
         ],
       ),
     );

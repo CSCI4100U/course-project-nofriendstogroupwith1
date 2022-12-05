ae27e918a987b273b4ed6d79cff859ece301bef5
Sukhpreet-7 <sukhpreet.bansal@ontariotechu.net>
2022-12-05 17:33:27 -0500

    Added to setting_view

--- group_project/lib/views/settings_view.dart
+++ group_project/lib/views/settings_view.dart
@@ -10,18 +10,21 @@ class SettingsView extends StatefulWidget {
 }
 
 class _SettingsViewState extends State<SettingsView> {
-
   final SettingsModel _settingsModel = SettingsModel();
 
   bool _autoSave = true;
   String _language = "en-US";
 
   Future<void> getUpdatedSettings() async {
-    _autoSave = await _settingsModel.getBoolSetting(SettingsModel.settingAutoSave) ?? true;
-    _language = await _settingsModel.getStringSetting(SettingsModel.settingLanguage) ?? 'en-US';
+    _autoSave =
+        await _settingsModel.getBoolSetting(SettingsModel.settingAutoSave) ??
+            true;
+    _language =
+        await _settingsModel.getStringSetting(SettingsModel.settingLanguage) ??
+            'en-US';
   }
 
-  Widget _buildSetting({String label="", required Widget child}) {
+  Widget _buildSetting({String label = "", required Widget child}) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
       child: Row(
@@ -34,7 +37,10 @@ class _SettingsViewState extends State<SettingsView> {
     );
   }
 
-  Widget _buildToggleSetting({String label="", bool value=false, required void Function(bool)? onChanged}) {
+  Widget _buildToggleSetting(
+      {String label = "",
+      bool value = false,
+      required void Function(bool)? onChanged}) {
     return _buildSetting(
       label: label,
       child: Switch(value: value, onChanged: onChanged),
@@ -43,65 +49,91 @@ class _SettingsViewState extends State<SettingsView> {
 
   Widget _buildSettingHeader(String _text) {
     return Padding(
-        padding: const EdgeInsets.only(top: 32.0, left: 16.0, bottom: 16.0, right: 16.0),
-        child: Text(_text, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),)
-    );
+        padding: const EdgeInsets.only(
+            top: 32.0, left: 16.0, bottom: 16.0, right: 16.0),
+        child: Text(
+          _text,
+          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ));
   }
-
 
   @override
   Widget build(BuildContext context) {
-
-
     return FutureBuilder(
-      future: getUpdatedSettings(),
-      builder: (context, snapshot) {
-        return ListView(
-          children: [
-            _buildSettingHeader("General"),
-            _buildToggleSetting(
-              label: "Auto-save added posts",
-              value: _autoSave,
-              onChanged: (value){
-                setState((){
-                  _autoSave=value;
-                  _settingsModel.setBoolSetting(name: SettingsModel.settingAutoSave, value: value);
-                });
-              }
-            ),
-
-            _buildSettingHeader("Localization"),
-            _buildSetting(
-              label: SettingsModel.settingLanguage,
-              child: DropdownButton(
-                value: _language,
-                items: const [
-                  DropdownMenuItem(
-                    value: "en-US",
-                    child: Text("🇺🇸 English (US)", textAlign: TextAlign.end,),
-                  ),
-                  DropdownMenuItem(
-                    value: "en-UK",
-                    child: Text("🇬🇧 English (UK)", textAlign: TextAlign.end,),
-                  ),
-                  DropdownMenuItem(
-                    value: "fr-FR",
-                    child: Text("🇫🇷 French (FR)", textAlign: TextAlign.end,),
-                  ),
-                ],
-                onChanged: (value){
-                  setState((){
-                    if (value!=null) {
-                      _language = value;
-                      _settingsModel.setStringSetting(name: SettingsModel.settingLanguage, value: value);
-                    }
-                  });
-                }
+        future: getUpdatedSettings(),
+        builder: (context, snapshot) {
+          return ListView(
+            children: [
+              _buildSettingHeader("General"),
+              _buildToggleSetting(
+                  label: "Auto-save added posts",
+                  value: _autoSave,
+                  onChanged: (value) {
+                    setState(() {
+                      _autoSave = value;
+                      _settingsModel.setBoolSetting(
+                          name: SettingsModel.settingAutoSave, value: value);
+                    });
+                  }),
+              _buildSettingHeader("Localization"),
+              _buildSetting(
+                label: SettingsModel.settingLanguage,
+                child: DropdownButton(
+                    value: _language,
+                    items: const [
+                      DropdownMenuItem(
+                        value: "en-US",
+                        child: Text(
+                          "🇺🇸 English (US)",
+                          textAlign: TextAlign.end,
+                        ),
+                      ),
+                      DropdownMenuItem(
+                        value: "en-UK",
+                        child: Text(
+                          "🇬🇧 English (UK)",
+                          textAlign: TextAlign.end,
+                        ),
+                      ),
+                      DropdownMenuItem(
+                        value: "fr-FR",
+                        child: Text(
+                          "🇫🇷 French (FR)",
+                          textAlign: TextAlign.end,
+                        ),
+                      ),
+                    ],
+                    onChanged: (value) {
+                      setState(() {
+                        if (value != null) {
+                          _language = value;
+                          _settingsModel.setStringSetting(
+                              name: SettingsModel.settingLanguage,
+                              value: value);
+                        }
+                      });
+                    }),
               ),
-            ),
-          ],
-        );
-      }
-    );
+              _buildSettingHeader("Legal"),
+              _buildSetting(
+                  label: "Details",
+                  child: ElevatedButton(
+                      onPressed: () {
+                        showAboutDialog(
+                          context: context,
+                          applicationName: "Post It, Pin It",
+                          applicationVersion: "4.1.2",
+                          children: [
+                            const Text(
+                                "\nThis app is created by Alexander Naylor, Dylan Moore, Sukhpreet Bansal and Hamza Khan."),
+                          ],
+                          applicationLegalese:
+                              "Copyright 2022 © Alexander Naylor, Dylan Moore, Sukhpreet Bansal and Hamza Khan.",
+                        );
+                      },
+                      child: const Text("About")))
+            ],
+          );
+        });
   }
 }
